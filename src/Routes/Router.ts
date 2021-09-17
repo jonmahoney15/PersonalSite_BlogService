756264e3c741f67dacf147816be7dcb65305aa4b
jonmahoney15 <jonmahoney15@yahoo.com>
2021-09-17 11:16:07 -0400

    Updated routes to new convention for paths

--- src/Routes/Router.ts
+++ src/Routes/Router.ts
@@ -3,13 +3,13 @@ import { GetPosts, CreatePost, RemovePost, UpdatePost } from "../blog/BlogContro
 import { getPost, auth, verifyAdmin, largeLimiter, upload } from "../Middleware";
 const router = Router();
 
-router.get("/contact/Contact", auth, (req: Request, res: Response) =>
-  res.status(200).json({ Status: "Success", message: "Contact is Healthy" })
+router.get("/blog/health", auth, (req: Request, res: Response) =>
+  res.status(200).json({ Status: "Success", message: "Blog service is Healthy" })
 );
 
-router.get("/blog/Posts", auth, largeLimiter, GetPosts);
-router.post("/blog/CreatePost", auth, verifyAdmin, upload.single("file"), CreatePost);
-router.post("/blog/RemovePost", auth, verifyAdmin, getPost, RemovePost);
-router.post("/blog/EditPost", auth, verifyAdmin, getPost, UpdatePost);
+router.get("/blog/posts", auth, largeLimiter, GetPosts);
+router.post("/blog/createpost", auth, verifyAdmin, upload.single("file"), CreatePost);
+router.post("/blog/removepost", auth, verifyAdmin, getPost, RemovePost);
+router.post("/blog/editpost", auth, verifyAdmin, getPost, UpdatePost);
 
 export { router as Router };
